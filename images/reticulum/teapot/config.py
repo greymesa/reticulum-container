36d7fa5e19af398b7ea66bf30249a9d68ba8cb4a
greymesa <58270183+greymesa@users.noreply.github.com>
2026-06-02 19:40:44 -0400

    remove teapot

--- images/reticulum/teapot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-RNS_CONFIGDIR = os.environ.get("RNS_CONFIG_PATH", "/config")
-STORAGE_PATH = os.path.join(RNS_CONFIGDIR, "storage")
-
-NODE_IDENTITY_PATH = os.path.join(RNS_CONFIGDIR, "identity")
-ANNOUNCE_NAME = os.environ.get("ANNOUNCE_NAME", "teapot")
-PAGES_DIRECTORY = os.environ.get("PAGES_DIRECTORY", "/usr/local/share/teapot")
--- images/reticulum/teapot/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-set -e
-source /setup
-if [ $# -ne 0 ]; then
-  exec "$@"
-fi
-mkdir -p "$RNS_CONFIG_PATH"/storage
-exec -a teapot teapot
